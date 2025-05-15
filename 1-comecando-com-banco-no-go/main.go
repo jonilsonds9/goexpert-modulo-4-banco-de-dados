756049e3f88981160f21485f76fa3dd2fd6d2241
Jonilson Sousa <jonilsonds9@gmail.com>
2025-05-15 19:58:33 -0300

    Selecionando múltiplos registros

--- 1-comecando-com-banco-no-go/main.go
+++ 1-comecando-com-banco-no-go/main.go
@@ -32,16 +32,26 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	product.Price = 100.0
 	err = updateProduct(db, product)
 	if err != nil {
 		panic(err)
 	}
-	p, err := selectOneProduct(db, product.ID)
+
+	//p, err := selectOneProduct(db, product.ID)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Printf("Product: %v, possui o preço de %.2f", p.Name, p.Price)
+
+	products, err := selectAllProducts(db)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Product: %v, possui o preço de %.2f", p.Name, p.Price)
+	for _, p := range products {
+		fmt.Printf("Product: %v, possui o preço de %.2f\n", p.Name, p.Price)
+	}
 }
 
 func insertProduct(db *sql.DB, product *Product) error {
@@ -82,4 +92,22 @@ func selectOneProduct(db *sql.DB, id string) (*Product, error) {
 		return nil, err
 	}
 	return &p, nil
+}
+
+func selectAllProducts(db *sql.DB) ([]Product, error) {
+	rows, err := db.Query("SELECT id, name, price FROM products")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var products []Product
+	for rows.Next() {
+		var p Product
+		err = rows.Scan(&p.ID, &p.Name, &p.Price)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+	return products, nil
 }
