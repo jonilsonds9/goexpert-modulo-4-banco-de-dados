1a589c253abcb30d3ad4758cd029748de82c16e5
Jonilson Sousa <jonilsonds9@gmail.com>
2025-05-16 11:43:20 -0300

    Belongs to

---
+++ 3-belongs-to/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Category struct {
+	ID   int `gorm:"primaryKey"`
+	Name string
+}
+
+type Product struct {
+	ID         int `gorm:"primaryKey"`
+	Name       string
+	Price      float64
+	CategoryID int
+	Category   Category
+	gorm.Model
+}
+
+func main() {
+	dsn := "root:@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&Product{}, &Category{})
+
+	// Create category
+	//category := Category{Name: "Electronicos"}
+	//db.Create(&category)
+
+	// Create product
+	db.Create(&Product{Name: "Mouse", Price: 1000.00, CategoryID: 1})
+
+	var products []Product
+	db.Preload("Category").Find(&products)
+	for _, product := range products {
+		fmt.Println(product.Name, product.Category.Name)
+	}
+}
